feb3ac8e251d587a81e82d773d5cdf4af358db45
meShahabaj <shahabaj773@gmail.com>
2025-12-03 20:52:11 +0530

    Refactor mailAdmin endpoint to use dynamic email recipient and message content

--- backend/server.js
+++ backend/server.js
@@ -21,37 +21,14 @@ app.post("/mailAdmin", async (req, res) => {
 
     const { name, email, message } = req.body;
     try {
-
-        // const transporter = nodemailer.createTransport(
-        //     {
-        //         service: "gmail",
-        //         auth: {
-        //             user: ADMIN_MAIL_1,
-        //             pass: ADMIN_MAIL_1_PASS
-        //         }
-        //     }
-        // )
         const resend = new Resend(process.env.RESEND_API);
 
         await resend.emails.send({
             from: 'onboarding@resend.dev',
-            to: 'shahabaj773@gmail.com',
-            subject: 'Hello World',
-            html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+            to: email,
+            subject: `${name} sent a message`,
+            html: `<p><strong>${message}</strong>!</p>`
         });
-
-        // const MailOptions = {
-        //     from: ADMIN_MAIL_1,
-        //     to: ADMIN_MAIL,
-        //     subject: `New message from ${name}`,
-        //     text: `
-        //     Name: ${name}
-        //     Email: ${email}
-
-        //     Message: ${message}
-        //         `,
-        // }
-        // await transporter.sendMail(MailOptions);
         res.json({ success: true, message: "Mail sent to admin" });
     } catch (err) {
         console.error(err);

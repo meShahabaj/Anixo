43a0dd44434d0d922eb6005ab7460026a85d5d8b
meShahabaj <shahabaj773@gmail.com>
2026-01-23 15:29:27 +0530

    fix: update Google Analytics ID variable in layout component

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.GA_ID}');
+            gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
           `}
         </Script>
       </head>
